36d488a60561448f074ace8ffd57a9a5ae0bb41e
CastalJaemin <ehdrud1332@gmail.com>
2020-09-26 11:39:54 +0900

    Navigation & Base Theme

---
+++ navigation/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+
+import theme from '../constants/theme'
+import Browse from "../screens/Browse";
+
+const screens = createStackNavigator(
+    {
+        Browse
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                height: theme.sizes.base * 4,
+                backgroundColor: theme.colors.white,
+                borderBottomColor: 'transparent',
+                elevation: 0
+            },
+            headerBackImage: <Image source={require('../assets/icons/back.png')}/>,
+            headerBackTitle: null,
+            headerLeftContainerStyle: {
+                alignItems: 'center',
+                marginLeft: theme.sizes.base * 2,
+                paddingRight: theme.sizes.base
+            },
+            headerRightContainerStyle: {
+                alignItems: 'center',
+                paddingRight: theme.sizes.base
+            }
+        }
+    }
+)
+
+export default createAppContainer(screens)
---
+++ screens/Browse.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+
+
+const Browse = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default Browse;
